d56c979cf37e398644f527c93f415277d025c50f
Sami <appdev@samichabbouh.fr>
2019-05-29 00:33:55 +0200

    deleted menu and added icons

--- src/features/bible/BibleNoteItem.js
+++ src/features/bible/BibleNoteItem.js
@@ -4,7 +4,6 @@ import { Icon } from 'expo'
 
 import Box from '~common/ui/Box'
 import Text from '~common/ui/Text'
-import { Menu } from 'react-native-paper'
 
 import Paragraph from '~common/ui/Paragraph'
 import theme from '~themes/default'
@@ -16,16 +15,11 @@ const NoteContainer = styled.View(({ theme }) => ({
 }))
 
 class BibleNoteItem extends React.Component {
-  state = {
-    isMenuOpen: false
-  }
-
   openModal = () => { this.setState({ isModalOpen: true }) }
   closeModal = () => { this.setState({ isMenuOpen: false }) }
 
   render () {
     const { item, openNoteEditor, deleteNote } = this.props
-    const { isMenuOpen } = this.state
 
     return (
       <NoteContainer style={{ marginBottom: 10 }}>
@@ -33,24 +27,22 @@ class BibleNoteItem extends React.Component {
           <Text color='darkGrey' bold fontSize={14}>
             {item.reference}
           </Text>
-          <Menu
-            visible={isMenuOpen}
-            onDismiss={this.closeModal}
-            anchor={
-              <Icon.Feather
-                name={'more-vertical'}
-                size={19}
-                style={{ paddingHorizontal: 10, paddingBottom: 5 }}
-                color={theme.colors.tertiary}
-                // onPress={openModal}
-                // onPress={() => openNoteEditor(item.noteId)}
-                onPress={() => deleteNote(item.noteId)}
-              />
-            }
-          >
-            <Menu.Item onPress={() => openNoteEditor(item.noteId)} title='Éditer' />
-            <Menu.Item onPress={() => deleteNote(item.noteId)} title='Supprimer' />
-          </Menu>
+          <Box row>
+            <Icon.Feather
+              name={'trash-2'}
+              size={15}
+              style={{ paddingHorizontal: 10, paddingBottom: 5 }}
+              color={theme.colors.tertiary}
+              onPress={() => deleteNote(item.noteId)}
+            />
+            <Icon.Feather
+              name={'edit-2'}
+              size={15}
+              style={{ paddingHorizontal: 10, paddingBottom: 5 }}
+              color={theme.colors.tertiary}
+              onPress={() => openNoteEditor(item.noteId)}
+            />
+          </Box>
         </Box>
         {!!item.notes.title && <Paragraph scale={-2} style={{ fontWeight: 'bold' }}>
           {item.notes.title}
