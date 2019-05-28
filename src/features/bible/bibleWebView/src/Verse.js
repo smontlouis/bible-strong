39662188786ebd046dc075ec0db906a683b90c6c
Stéphane Montlouis-Calixte <s.montlouis.calixte@gmail.com>
2019-05-28 21:49:08 +1200

    Android does not support hexa 6 char

--- src/features/bible/bibleWebView/src/Verse.js
+++ src/features/bible/bibleWebView/src/Verse.js
@@ -3,9 +3,18 @@ import picostyle from 'picostyle'
 
 import { dispatch, NAVIGATE_TO_BIBLE_VERSE_DETAIL,
   NAVIGATE_TO_VERSE_NOTES, TOGGLE_SELECTED_VERSE } from './dispatch'
-import colors from '../../../../themes/colors'
-import hexTransarency from '../../../../helpers/hexTransarency'
+import { getColors } from '../../../../themes/getColors'
 import NotesCount from './NotesCount'
+
+function convertHex (hex, opacity) {
+  hex = hex.replace('#', '')
+  const r = parseInt(hex.substring(0, 2), 16)
+  const g = parseInt(hex.substring(2, 4), 16)
+  const b = parseInt(hex.substring(4, 6), 16)
+
+  const result = 'rgba(' + r + ',' + g + ',' + b + ',' + opacity / 100 + ')'
+  return result
+}
 
 const styled = picostyle(h)
 
@@ -21,10 +30,10 @@ const NumberText = styled('span')(({ settings: { fontSizeScale } }) => ({
   padding: '0 10px'
 }))
 
-const ContainerText = styled('span')(({ isFocused, isSelected, highlightedColor }) => {
+const ContainerText = styled('span')(({ isFocused, isSelected, highlightedColor, settings: { theme } }) => {
   let background = 'transparent'
   if (highlightedColor && !isSelected) {
-    background = `${colors[highlightedColor]}${hexTransarency[50]}`
+    background = `${convertHex(getColors[theme][highlightedColor], 50)}`
   }
   if (isFocused) {
     background = 'rgba(0,0,0,0.1)'
@@ -86,6 +95,7 @@ class Verse extends Component {
     return (
       <Wrapper settings={settings} id={`verset-${verse.Verset}`}>
         <ContainerText
+          settings={settings}
           isFocused={isFocused}
           isSelected={isSelected}
           highlightedColor={highlightedColor}
