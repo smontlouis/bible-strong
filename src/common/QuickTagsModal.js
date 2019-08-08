5e7b6eadb72e7d565c3b46fdc3e47f2b8efc2131
Stéphane Montlouis-Calixte <s.montlouis.calixte@gmail.com>
2019-08-08 17:05:13 +1200

    Fix quickmodal for android

--- src/common/QuickTagsModal.js
+++ src/common/QuickTagsModal.js
@@ -3,19 +3,18 @@ import Modal from 'react-native-modal'
 import styled from '@emotion/native'
 import * as Icon from '@expo/vector-icons'
 import { withTheme } from 'emotion-theming'
+import { getBottomSpace } from 'react-native-iphone-x-helper'
 
-import Box from '~common/ui/Box'
 import AnimatedCircularProgress from '~common/AnimatedCircularProgress'
-import { TouchableOpacity } from 'react-native-gesture-handler'
 
 const StylizedModal = styled(Modal)({
   justifyContent: 'flex-end',
   alignItems: 'center',
   margin: 0,
-  paddingBottom: 100
+  paddingBottom: 60 + getBottomSpace()
 })
 
-const WrapperIcon = styled(Box)(({ theme }) => ({
+const WrapperIcon = styled.TouchableOpacity(({ theme }) => ({
   width: 50,
   height: 50,
   borderRadius: 25,
@@ -50,33 +49,31 @@ const QuickTagsModal = ({ theme, item, onClosed, setMultipleTagsItem }) => {
       onBackdropPress={onClosed}
       avoidKeyboard
     >
-      <TouchableOpacity
-        onPress={() => {
-          onClosed()
-          setTimeout(() => {
-            setMultipleTagsItem(item)
-          }, 500)
-        }}
-      >
 
-        <AnimatedCircularProgress
-          ref={progressRef}
-          size={60}
-          width={5}
-          fill={100}
-          tintColor='#46DF4C'
-          onAnimationComplete={onClosed}
-          lineCap='round'
-        >
-          {
-            () => (
-              <WrapperIcon>
-                <StyledIcon size={20} name='tags' />
-              </WrapperIcon>
-            )
-          }
-        </AnimatedCircularProgress>
-      </TouchableOpacity>
+      <AnimatedCircularProgress
+        ref={progressRef}
+        size={60}
+        width={5}
+        fill={100}
+        tintColor='#46DF4C'
+        onAnimationComplete={onClosed}
+        lineCap='round'
+      >
+        {
+          () => (
+            <WrapperIcon
+              onPress={() => {
+                onClosed()
+                setTimeout(() => {
+                  setMultipleTagsItem(item)
+                }, 500)
+              }}
+            >
+              <StyledIcon size={20} name='tags' />
+            </WrapperIcon>
+          )
+        }
+      </AnimatedCircularProgress>
 
     </StylizedModal>
   )
