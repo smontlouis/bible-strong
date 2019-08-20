b2f244f9d9a1fef245ea66fcde9814e56e497c98
Stéphane Montlouis-Calixte <s.montlouis.calixte@gmail.com>
2019-08-20 20:53:23 +1200

    Remove useless console.log

--- src/features/dictionnary/DictionnaryDetailScreen.js
+++ src/features/dictionnary/DictionnaryDetailScreen.js
@@ -35,8 +35,6 @@ const DictionnaryDetailScreen = ({ navigation }) => {
     loadDictionnaireItem(word).then(result => setDictionnaireItem(result))
   }, [word])
 
-  console.log(dictionnaireItem)
-
   const openLink = (href, content, type) => {
     if (type === 'verse') {
       try {
