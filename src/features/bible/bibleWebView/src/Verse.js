7ccb66eca80ae0dcfc1847848a5eab7b8470a90e
Stéphane Montlouis-Calixte <s.montlouis.calixte@gmail.com>
2019-09-04 00:49:35 +1200

    linear background to 60%

--- src/features/bible/bibleWebView/src/Verse.js
+++ src/features/bible/bibleWebView/src/Verse.js
@@ -47,10 +47,10 @@ const ContainerText = styled('span')(
   ({ isFocused, isSelected, highlightedColor, isVerseToScroll, settings: { theme, colors } }) => {
     let background = 'transparent'
     if (highlightedColor && !isSelected) {
-      background = `linear-gradient(180deg, rgba(255,255,255,0) 55%, ${convertHex(
+      background = `linear-gradient(180deg, rgba(255,255,255,0) 60%, ${convertHex(
         colors[theme][highlightedColor],
         50
-      )} 55%)`
+      )} 60%)`
     }
     if (isFocused) {
       background = 'rgba(0,0,0,0.1)'
