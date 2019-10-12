3e819fa555fbd621f2359cad510a8d69566db9a0
Stéphane Montlouis-Calixte <s.montlouis.calixte@gmail.com>
2019-10-13 12:41:52 +1300

    Fix issue with dictionnaire dl

--- src/common/waitForDictionnaireDB.js
+++ src/common/waitForDictionnaireDB.js
@@ -57,79 +57,63 @@ export const useWaitForDatabase = () => {
             throw new Error('SQLite dir is not a directory')
           }
 
-          if (sqliteDB.localUri) {
-            await FileSystem.copyAsync({
-              from: sqliteDB.localUri,
-              to: dbPath
-            })
-
-            await initDictionnaireDB()
-
-            console.log('DB dictionnaire loaded')
-
+          // Waiting for user to accept to download
+          if (!startDownload) {
             dispatch({
-              type: 'dictionnaire.setLoading',
-              payload: false
+              type: 'dictionnaire.setProposeDownload',
+              payload: true
             })
-          } else {
-            // Waiting for user to accept to download
-            if (!startDownload) {
-              dispatch({
-                type: 'dictionnaire.setProposeDownload',
-                payload: true
-              })
-              return
-            }
+            return
+          }
 
-            try {
-              if (!window.dictionnaireDownloadHasStarted) {
-                window.dictionnaireDownloadHasStarted = true
-                console.log(`Downloading ${sqliteDB.uri} to ${dbPath}`)
+          try {
+            if (!window.dictionnaireDownloadHasStarted) {
+              window.dictionnaireDownloadHasStarted = true
+              console.log(`Downloading ${sqliteDB.uri} to ${dbPath}`)
 
-                if (!sqliteDir.exists) {
-                  await FileSystem.makeDirectoryAsync(sqliteDirPath)
-                } else if (!sqliteDir.isDirectory) {
-                  throw new Error('SQLite dir is not a directory')
-                }
-
-                await FileSystem.createDownloadResumable(
-                  sqliteDB.uri,
-                  dbPath,
-                  null,
-                  ({ totalBytesWritten, totalBytesExpectedToWrite }) => {
-                    const idxProgress =
-                      Math.floor((totalBytesWritten / DICTIONNAIRE_FILE_SIZE) * 100) / 100
-                    dispatch({
-                      type: 'dictionnaire.setProgress',
-                      payload: idxProgress
-                    })
-                  }
-                ).downloadAsync()
-
-                dispatchRedux(setDictionnaireDatabaseHash(sqliteDB.hash))
-
-                await initDictionnaireDB()
-                console.log('DB dictionnaire loaded')
-                dispatch({
-                  type: 'dictionnaire.setLoading',
-                  payload: false
-                })
-                window.dictionnaireDownloadHasStarted = false
+              if (!sqliteDir.exists) {
+                await FileSystem.makeDirectoryAsync(sqliteDirPath)
+              } else if (!sqliteDir.isDirectory) {
+                throw new Error('SQLite dir is not a directory')
               }
-            } catch (e) {
-              SnackBar.show(
-                "Impossible de commencer le téléchargement. Assurez-vous d'être connecté à internet.",
-                'danger'
-              )
+
+              await FileSystem.createDownloadResumable(
+                sqliteDB.uri,
+                dbPath,
+                null,
+                ({ totalBytesWritten, totalBytesExpectedToWrite }) => {
+                  const idxProgress =
+                    Math.floor((totalBytesWritten / DICTIONNAIRE_FILE_SIZE) * 100) / 100
+                  dispatch({
+                    type: 'dictionnaire.setProgress',
+                    payload: idxProgress
+                  })
+                }
+              ).downloadAsync()
+
+              dispatchRedux(setDictionnaireDatabaseHash(sqliteDB.hash))
+
+              await initDictionnaireDB()
+              console.log('DB dictionnaire loaded')
               dispatch({
-                type: 'dictionnaire.setProposeDownload',
-                payload: true
-              })
-              dispatch({
-                type: 'dictionnaire.setStartDownload',
+                type: 'dictionnaire.setLoading',
                 payload: false
               })
+              window.dictionnaireDownloadHasStarted = false
             }
+          } catch (e) {
+            SnackBar.show(
+              "Impossible de commencer le téléchargement. Assurez-vous d'être connecté à internet.",
+              'danger'
+            )
+            dispatch({
+              type: 'dictionnaire.setProposeDownload',
+              payload: true
+            })
+            dispatch({
+              type: 'dictionnaire.setStartDownload',
+              payload: false
+            })
           }
         } else {
           await initDictionnaireDB()
