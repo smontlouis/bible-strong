00df7f601e37f9846a3e7f025f08775ecc3602ce
bulby97 <s.montlouis.calixte@gmail.com>
2020-05-23 08:06:02 +1200

    Use rn iap

--- ios/Pods/Headers/Public/EXInAppPurchases/EXInAppPurchasesModule.h
+++
@@ -1,1 +0,0 @@
-../../../../../node_modules/expo-in-app-purchases/ios/EXInAppPurchases/EXInAppPurchasesModule.h
--- src/helpers/useInAppPurchases.ts
+++ src/helpers/useInAppPurchases.ts
@@ -1,29 +1,13 @@
 import { useEffect, useState } from 'react'
-import {
-  IAPResponseCode,
-  connectAsync,
-  getProductsAsync,
-  setPurchaseListener,
-  finishTransactionAsync,
-  InAppPurchase,
-  IAPItemDetails,
-} from 'expo-in-app-purchases'
 import { Platform } from 'react-native'
 import { Status } from '~common/types'
+import { Product, initConnection, getSubscriptions } from 'react-native-iap'
+import to from 'await-to-js'
 
-export const productsDetails =
-  Platform.OS === 'ios'
-    ? [
-        'com.smontlouis.biblestrong.onemonth',
-        'com.smontlouis.biblestrong.threemonths',
-        'com.smontlouis.biblestrong.oneyear',
-      ]
-    : [
-        'dev.products.gas',
-        'dev.products.premium',
-        'dev.products.gold_monthly',
-        'dev.products.gold_yearly',
-      ]
+const itemSkus = Platform.select({
+  ios: ['com.smontlouis.biblestrong.dev.onemonth'],
+  android: ['com.example.coins100'],
+})
 
 export const useIAP = () => {
   const isInitIAP = useInitInAppPurchases()
@@ -34,15 +18,8 @@ export const useInitInAppPurchases = () => {
   const [isInitIAP, setInitIAP] = useState(false)
   useEffect(() => {
     ;(async () => {
-      const history = await connectAsync()
-      if (history.responseCode === IAPResponseCode.OK) {
-        const { responseCode, results } = await getProductsAsync(
-          productsDetails
-        )
-        if (responseCode === IAPResponseCode.OK) {
-          console.log(results)
-        }
-      }
+      const [err] = await to(initConnection())
+      console.log('IAP initialized')
       setInitIAP(true)
     })()
   }, [])
@@ -50,55 +27,56 @@ export const useInitInAppPurchases = () => {
   return isInitIAP
 }
 
-export const usePurchaseListener = () => {
-  useEffect(() => {
-    ;(async () => {
-      // Set purchase listener
-      setPurchaseListener(({ responseCode, results, errorCode }) => {
-        // Purchase was successful
-        if (responseCode === IAPResponseCode.OK) {
-          results.forEach(purchase => {
-            if (!purchase.acknowledged) {
-              console.log(`Successfully purchased ${purchase.productId}`)
-              // Process transaction here and unlock content...
+// export const usePurchaseListener = () => {
+//   useEffect(() => {
+//     ;(async () => {
+//       // Set purchase listener
+//       setPurchaseListener(({ responseCode, results, errorCode }) => {
+//         // Purchase was successful
+//         if (responseCode === IAPResponseCode.OK) {
+//           results.forEach(purchase => {
+//             if (!purchase.acknowledged) {
+//               console.log(`Successfully purchased ${purchase.productId}`)
+//               // Process transaction here and unlock content...
 
-              // Then when you're done
-              finishTransactionAsync(purchase, true)
-            }
-          })
-        }
+//               // Then when you're done
+//               finishTransactionAsync(purchase, true)
+//             }
+//           })
+//         }
 
-        // Else find out what went wrong
-        if (responseCode === IAPResponseCode.USER_CANCELED) {
-          console.log('User canceled the transaction')
-        } else if (responseCode === IAPResponseCode.DEFERRED) {
-          console.log(
-            'User does not have permissions to buy but requested parental approval (iOS only)'
-          )
-        } else {
-          console.warn(
-            `Something went wrong with the purchase. Received errorCode ${errorCode}`
-          )
-        }
-      })
-    })()
-  }, [])
-}
+//         // Else find out what went wrong
+//         if (responseCode === IAPResponseCode.USER_CANCELED) {
+//           console.log('User canceled the transaction')
+//         } else if (responseCode === IAPResponseCode.DEFERRED) {
+//           console.log(
+//             'User does not have permissions to buy but requested parental approval (iOS only)'
+//           )
+//         } else {
+//           console.warn(
+//             `Something went wrong with the purchase. Received errorCode ${errorCode}`
+//           )
+//         }
+//       })
+//     })()
+//   }, [])
+// }
 
 export const useProducts = (isInitIAP: boolean) => {
-  const [products, setProducts] = useState<(InAppPurchase | IAPItemDetails)[]>(
-    []
-  )
+  const [products, setProducts] = useState<Product[]>([])
   const [status, setStatus] = useState<Status>('Idle')
 
   useEffect(() => {
     ;(async () => {
       if (!isInitIAP) return
       setStatus('Pending')
-      const { responseCode, results } = await getProductsAsync(productsDetails)
-      console.log(responseCode, results)
-      if (responseCode === IAPResponseCode.OK && results) {
-        setProducts(results)
+      const [err, productItems] = await to<Product[]>(
+        getSubscriptions(itemSkus)
+      )
+
+      console.log(productItems)
+      if (!err && productItems) {
+        setProducts(productItems)
         setStatus('Resolved')
       } else {
         setStatus('Rejected')
