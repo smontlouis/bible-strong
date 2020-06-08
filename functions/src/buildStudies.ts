ea6bd8db85440a899d3ce7c90e1f57e8508de76f
bulby97 <s.montlouis.calixte@gmail.com>
2020-06-08 17:19:44 +1200

    Update

---
+++ functions/src/buildStudies.ts
@@ -0,0 +1,18 @@
+import fetch from 'node-fetch'
+import * as functions from 'firebase-functions'
+
+export const buildStudies = functions.firestore
+  .document('studies/{studyId}')
+  .onUpdate(async (change, context) => {
+    const newValue = change.after.data()
+    const previousValue = change.before.data()
+
+    if (newValue.published !== previousValue.published) {
+      await fetch(
+        'https://api.zeit.co/v1/integrations/deploy/QmPr9jhLF1bhDakSDDFuLvbax4VCogXsjqzktsmTHDUMXG/FbjfTqbcO7',
+        {
+          method: 'POST',
+        }
+      )
+    }
+  })
