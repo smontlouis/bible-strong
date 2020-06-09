1e96135ebf994de93d5c6fbf007af22aa449f7b9
bulby97 <s.montlouis.calixte@gmail.com>
2020-06-09 16:19:40 +1200

    Fix issues with pdf

---
+++ functions/src/export-study-pdf.ts
@@ -0,0 +1,40 @@
+import * as functions from 'firebase-functions'
+import * as puppeteer from 'puppeteer'
+
+const cors = require('cors')({ origin: true })
+
+const runtimeOpts = {
+  memory: '1GB' as '1GB',
+}
+
+export const exportStudyPDF = functions
+  .runWith(runtimeOpts)
+  .https.onRequest((req, res) => {
+    cors(req, res, async () => {
+      try {
+        console.log('---start')
+        const { studyId } = req.body
+        const browser = await puppeteer.launch({
+          args: ['--disable-gpu', '--no-sandbox', '--disable-dev-shm-usage'],
+        })
+        const page = await browser.newPage()
+
+        await page.goto(
+          `https://bible-strong-web-app.now.sh/studies/${studyId}`,
+          {
+            waitUntil: 'networkidle0',
+          }
+        )
+
+        const buffer = await page.pdf({ format: 'A4' })
+        console.log('---end')
+
+        await page.close()
+        res.send(buffer)
+      } catch (error) {
+        console.log('---failure')
+        console.log(error)
+        res.status(500).send(error)
+      }
+    })
+  })
