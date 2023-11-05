8fe5e97b481ee58d0afa5210d43c61a231b3f1cc
smontlouis <s.montlouis.calixte@gmail.com>
2023-11-05 15:49:59 +0100

    chore: remove plans and bible_versions from gitignore

---
+++ src/features/timeline/events.ts
@@ -0,0 +1,18 @@
+import { TimelineSection, TimelineEvent } from './types'
+import { Asset } from 'expo-asset'
+import * as FileSystem from 'expo-file-system'
+
+// Manually unfixed : 1242 - 1244
+// export const events: TimelineSection[] = require('~assets/timeline/events.txt')
+const events = require('~assets/timeline/events.txt')
+
+export const getEvents = async () => {
+  const [{ localUri }] = await Asset.loadAsync(
+    require('~assets/timeline/events.txt')
+  )
+
+  const json = JSON.parse(
+    await FileSystem.readAsStringAsync(localUri as string)
+  )
+  return json as TimelineSection[]
+}
