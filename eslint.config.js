aedefabdf980a01e1852c3aae3e054965eb3efd6
smontlouis <s.montlouis.calixte@gmail.com>
2025-12-17 18:09:44 +0100

    fix: add eslint 9

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+const expoConfig = require('eslint-config-expo/flat')
+const prettierPlugin = require('eslint-plugin-prettier')
+const prettierConfig = require('eslint-config-prettier')
+
+module.exports = [
+  ...expoConfig,
+  prettierConfig,
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  },
+  {
+    ignores: [
+      'node_modules/**',
+      '.expo/**',
+      'android/**',
+      'ios/**',
+      'builds/**',
+      'dist/**',
+      '*.config.js',
+      'babel.config.js',
+      'metro.config.js',
+      'eslint.config.js',
+      'src/features/bible/bibleWebView/**',
+      'src/features/studies/studiesWebView/**',
+      'src/helpers/react-native-htmlview/vendor/**',
+      'src/helpers/lunr.*.min.js',
+    ],
+  },
+]
