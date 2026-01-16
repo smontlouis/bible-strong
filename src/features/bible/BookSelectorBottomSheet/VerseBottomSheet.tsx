453bc3f833eab3bf43b370296e3030cda667d43f
smontlouis <s.montlouis.calixte@gmail.com>
2026-01-16 06:57:52 +0100

    fix: issue with verse bottom sheet

--- src/features/bible/BookSelectorBottomSheet/VerseBottomSheet.tsx
+++ src/features/bible/BookSelectorBottomSheet/VerseBottomSheet.tsx
@@ -1,19 +1,18 @@
-import BottomSheet, { BottomSheetScrollView, BottomSheetView } from '@gorhom/bottom-sheet'
-import { Portal } from '@gorhom/portal'
+import BottomSheet, { BottomSheetScrollView } from '@gorhom/bottom-sheet'
 import { useAtomValue } from 'jotai/react'
 import { atom } from 'jotai/vanilla'
-import React, { useCallback } from 'react'
+import React from 'react'
 import { useTranslation } from 'react-i18next'
-import { FlatList, useWindowDimensions } from 'react-native'
+import { useWindowDimensions } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import { BibleTab, BibleTabActions } from 'src/state/tabs'
 import { Book } from '~assets/bible_versions/books-desc'
 import { Verse } from '~common/types'
-import Box, { HStack, TouchableBox } from '~common/ui/Box'
+import Box, { TouchableBox } from '~common/ui/Box'
 import Text from '~common/ui/Text'
 import { useOpenInNewTab } from '~features/app-switcher/utils/useOpenInNewTab'
-import generateUUID from '~helpers/generateUUID'
 import { renderBackdrop, useBottomSheetStyles } from '~helpers/bottomSheetHelpers'
+import generateUUID from '~helpers/generateUUID'
 import loadBibleChapter from '~helpers/loadBibleChapter'
 import { useQuery } from '~helpers/react-query-lite'
 
@@ -50,7 +49,7 @@ const VerseBottomSheet = ({
   const { selectedVersion: version } = data || {}
   const { setTempSelectedVerse, validateTempSelected } = actions || {}
 
-  const { data: verses } = useQuery({
+  const { data: bibleChapterResult } = useQuery({
     queryKey: [
       'bible',
       version || '',
@@ -58,19 +57,18 @@ const VerseBottomSheet = ({
       tempSelectedChapter?.toString() || '',
     ],
     queryFn: () =>
-      loadBibleChapter(tempSelectedBook?.Numero, tempSelectedChapter, version) as Promise<Verse[]>,
+      loadBibleChapter(tempSelectedBook?.Numero || 1, tempSelectedChapter || 1, version || 'LSG'),
     enabled: !!tempSelectedBook && !!tempSelectedChapter && !!version,
   })
 
-  const handleSelect = useCallback(
-    (verse: Verse) => {
-      setTempSelectedVerse?.(Number(verse.Verset))
-      validateTempSelected?.()
-      bottomSheetRef.current?.close()
-      bookSelectorRef.current?.close()
-    },
-    [setTempSelectedVerse, validateTempSelected]
-  )
+  const verses = bibleChapterResult?.data
+
+  const handleSelect = (verse: Verse) => {
+    setTempSelectedVerse?.(Number(verse.Verset))
+    validateTempSelected?.()
+    bottomSheetRef.current?.close()
+    bookSelectorRef.current?.close()
+  }
 
   const handleLongPress = (verse: Verse) => {
     bottomSheetRef.current?.close()
