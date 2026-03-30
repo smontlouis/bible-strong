97bbb6e74a27e76a8a75bbd615c4cd1e9ae68182
smontlouis <s.montlouis.calixte@gmail.com>
2026-03-30 10:27:59 +0200

    chore: increment version

--- app.config.ts
+++ app.config.ts
@@ -9,14 +9,14 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   primaryColor: '#ffffff',
   githubUrl: 'https://github.com/bulby97/bible-strong',
   platforms: ['ios', 'android'],
-  version: '25.0.6',
+  version: '25.0.7',
   orientation: 'default',
   icon: './assets/images/icon-2.png',
   userInterfaceStyle: 'automatic',
   newArchEnabled: true,
 
   android: {
-    versionCode: 460,
+    versionCode: 461,
     package: 'com.smontlouis.biblestrong',
     googleServicesFile:
       process.env.ANDROID_GOOGLE_SERVICES_FILE ?? 'firebase/dev/google-services.json',
@@ -43,7 +43,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   },
   ios: {
     bundleIdentifier: process.env.BUNDLE_IDENTIFIER ?? 'com.smontlouis.biblestrong.dev',
-    buildNumber: '249',
+    buildNumber: '250',
     googleServicesFile:
       process.env.IOS_GOOGLE_SERVICES_FILE ?? './firebase/dev/GoogleService-Info.plist',
     userInterfaceStyle: 'automatic',
